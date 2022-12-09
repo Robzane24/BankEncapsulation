44f0c98348ba05e54dee5fd884ef4cad931193f5
Robert Zane <rob.zane24@gmail.com>
2022-12-09 10:37:59 -0600

    Completed BankEncapsulation

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var account = new BankAccount();
+            Console.WriteLine("How much money would you like to deposit?");
+
+            var depositAmount = double.Parse(Console.ReadLine());
+
+            account.Deposit(depositAmount);
+
+            Console.WriteLine($"Your balance is now {account.GetBalance()}");
         }
     }
 }
